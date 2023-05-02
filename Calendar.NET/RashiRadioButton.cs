11011c922ad82a7c6926984e4a7b581a2aab4565
문현지 <hjmoon@wemaginesoft.co.kr>
2023-05-02 09:54:49 +0900

    용수 시험자 일정 주기설정 팝업 작업

---
+++ Calendar.NET/RashiRadioButton.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace Calendar.NETDemo
+{
+    class RashiRadioButton : RadioButton
+    {
+
+        //Fields
+
+        private Color checkColor = Color.LightSeaGreen;
+        private Color unCheckColor = Color.Gray;
+
+        public Color CheckColor
+        {
+            get
+            {
+                return checkColor;
+            }
+            set
+            {
+                checkColor = value;
+                this.Invalidate();
+            }
+        }
+        public Color UnCheckColor
+        {
+            get
+            {
+                return unCheckColor;
+            }
+            set
+            {
+                unCheckColor = value;
+                this.Invalidate();
+            }
+        }
+
+
+
+
+        public RashiRadioButton()
+        {
+            this.MinimumSize = new Size(0, 20);
+        }
+
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            Graphics graphics = pevent.Graphics;
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            float bordersize = 19f;
+            float checkedsize = 13f;
+
+            RectangleF rectangleBorder = new RectangleF()
+            {
+                X = 0.6f,
+                Y = (this.Height - bordersize) / 2,
+                Width = bordersize,
+                Height = bordersize,
+            };
+
+            RectangleF rectangleChecked = new RectangleF()
+            {
+                X = rectangleBorder.X + ((rectangleBorder.Width - checkedsize) / 2),
+                Y = (this.Height - checkedsize) / 2,
+                Width = checkedsize,
+                Height = checkedsize
+            };
+
+
+            using (Pen PBorder = new Pen(CheckColor, 1.7f))
+            using (SolidBrush solidBrush = new SolidBrush(CheckColor))
+            using (SolidBrush Btext = new SolidBrush(this.ForeColor))
+            {
+                graphics.Clear(this.BackColor);
+                if (this.Checked)
+                {
+                    graphics.DrawEllipse(PBorder, rectangleBorder);
+                    graphics.FillEllipse(solidBrush, rectangleChecked);
+                }
+                else
+                {
+                    PBorder.Color = unCheckColor;
+                    graphics.DrawEllipse(PBorder, rectangleBorder);
+                }
+
+                graphics.DrawString(this.Text, this.Font, Btext,
+                bordersize + 8, (this.Height - TextRenderer.MeasureText(this.Text, this.Font).Height) / 2);
+
+
+            }
+
+
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            this.Width = TextRenderer.MeasureText(this.Text, this.Font).Width + 30;
+        }
+
+
+    }
+}
--- Calendar.NET/ScheduleEvent.Designer.cs
+++ Calendar.NET/ScheduleEvent.Designer.cs
@@ -29,45 +29,347 @@ namespace Calendar.NET
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.monthCalendar2 = new System.Windows.Forms.MonthCalendar();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rashiRadioButton1 = new Calendar.NETDemo.RashiRadioButton();
+            this.rashiRadioButton2 = new Calendar.NETDemo.RashiRadioButton();
+            this.rashiRadioButton3 = new Calendar.NETDemo.RashiRadioButton();
+            this.rashiRadioButton4 = new Calendar.NETDemo.RashiRadioButton();
+            this.rashiRadioButton5 = new Calendar.NETDemo.RashiRadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.monthCalendar2);
+            this.groupBox2.Controls.Add(this.monthCalendar1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(550, 260);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "기간";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(385, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 15);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "끝 날짜";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(107, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "시작날짜";
+            // 
+            // monthCalendar2
+            // 
+            this.monthCalendar2.Location = new System.Drawing.Point(290, 44);
+            this.monthCalendar2.Name = "monthCalendar2";
+            this.monthCalendar2.TabIndex = 1;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(12, 44);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 290);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(271, 167);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "순서";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "시험자를 선택하세요.",
+            "희해",
+            "아라",
+            "서영",
+            "미래"});
+            this.comboBox4.Location = new System.Drawing.Point(44, 121);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 23);
+            this.comboBox4.TabIndex = 7;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "시험자를 선택하세요.",
+            "희해",
+            "아라",
+            "서영",
+            "미래"});
+            this.comboBox3.Location = new System.Drawing.Point(44, 92);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 23);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "시험자를 선택하세요.",
+            "희해",
+            "아라",
+            "서영",
+            "미래"});
+            this.comboBox2.Location = new System.Drawing.Point(44, 62);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 5;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(198, 50);
+            this.comboBox1.Items.AddRange(new object[] {
+            "시험자를 선택하세요.",
+            "희해",
+            "아라",
+            "서영",
+            "미래"});
+            this.comboBox1.Location = new System.Drawing.Point(44, 33);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(203, 23);
-            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 4;
             // 
-            // textBox1
+            // label4
             // 
-            this.textBox1.Location = new System.Drawing.Point(301, 79);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 25);
-            this.textBox1.TabIndex = 1;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "4.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 31);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "취소";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(346, 500);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 31);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "확인";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rashiRadioButton5);
+            this.groupBox3.Controls.Add(this.rashiRadioButton4);
+            this.groupBox3.Controls.Add(this.rashiRadioButton3);
+            this.groupBox3.Controls.Add(this.rashiRadioButton2);
+            this.groupBox3.Controls.Add(this.rashiRadioButton1);
+            this.groupBox3.Location = new System.Drawing.Point(302, 290);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(260, 167);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "색상";
+            // 
+            // rashiRadioButton1
+            // 
+            this.rashiRadioButton1.AutoSize = true;
+            this.rashiRadioButton1.CheckColor = System.Drawing.Color.LightSeaGreen;
+            this.rashiRadioButton1.Location = new System.Drawing.Point(30, 54);
+            this.rashiRadioButton1.MinimumSize = new System.Drawing.Size(0, 20);
+            this.rashiRadioButton1.Name = "rashiRadioButton1";
+            this.rashiRadioButton1.Size = new System.Drawing.Size(157, 20);
+            this.rashiRadioButton1.TabIndex = 0;
+            this.rashiRadioButton1.TabStop = true;
+            this.rashiRadioButton1.Text = "rashiRadioButton1";
+            this.rashiRadioButton1.UnCheckColor = System.Drawing.Color.Gray;
+            this.rashiRadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // rashiRadioButton2
+            // 
+            this.rashiRadioButton2.AutoSize = true;
+            this.rashiRadioButton2.CheckColor = System.Drawing.Color.LightSeaGreen;
+            this.rashiRadioButton2.Location = new System.Drawing.Point(30, 80);
+            this.rashiRadioButton2.MinimumSize = new System.Drawing.Size(0, 20);
+            this.rashiRadioButton2.Name = "rashiRadioButton2";
+            this.rashiRadioButton2.Size = new System.Drawing.Size(157, 20);
+            this.rashiRadioButton2.TabIndex = 1;
+            this.rashiRadioButton2.TabStop = true;
+            this.rashiRadioButton2.Text = "rashiRadioButton2";
+            this.rashiRadioButton2.UnCheckColor = System.Drawing.Color.Gray;
+            this.rashiRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // rashiRadioButton3
+            // 
+            this.rashiRadioButton3.AutoSize = true;
+            this.rashiRadioButton3.CheckColor = System.Drawing.Color.LightSeaGreen;
+            this.rashiRadioButton3.Location = new System.Drawing.Point(30, 106);
+            this.rashiRadioButton3.MinimumSize = new System.Drawing.Size(0, 20);
+            this.rashiRadioButton3.Name = "rashiRadioButton3";
+            this.rashiRadioButton3.Size = new System.Drawing.Size(157, 20);
+            this.rashiRadioButton3.TabIndex = 2;
+            this.rashiRadioButton3.TabStop = true;
+            this.rashiRadioButton3.Text = "rashiRadioButton3";
+            this.rashiRadioButton3.UnCheckColor = System.Drawing.Color.Gray;
+            this.rashiRadioButton3.UseVisualStyleBackColor = true;
+            // 
+            // rashiRadioButton4
+            // 
+            this.rashiRadioButton4.AutoSize = true;
+            this.rashiRadioButton4.CheckColor = System.Drawing.Color.LightSeaGreen;
+            this.rashiRadioButton4.Location = new System.Drawing.Point(30, 132);
+            this.rashiRadioButton4.MinimumSize = new System.Drawing.Size(0, 20);
+            this.rashiRadioButton4.Name = "rashiRadioButton4";
+            this.rashiRadioButton4.Size = new System.Drawing.Size(157, 20);
+            this.rashiRadioButton4.TabIndex = 3;
+            this.rashiRadioButton4.TabStop = true;
+            this.rashiRadioButton4.Text = "rashiRadioButton4";
+            this.rashiRadioButton4.UnCheckColor = System.Drawing.Color.Gray;
+            this.rashiRadioButton4.UseVisualStyleBackColor = true;
+            // 
+            // rashiRadioButton5
+            // 
+            this.rashiRadioButton5.AutoSize = true;
+            this.rashiRadioButton5.CheckColor = System.Drawing.Color.LightSeaGreen;
+            this.rashiRadioButton5.Location = new System.Drawing.Point(30, 28);
+            this.rashiRadioButton5.MinimumSize = new System.Drawing.Size(0, 20);
+            this.rashiRadioButton5.Name = "rashiRadioButton5";
+            this.rashiRadioButton5.Size = new System.Drawing.Size(157, 20);
+            this.rashiRadioButton5.TabIndex = 4;
+            this.rashiRadioButton5.TabStop = true;
+            this.rashiRadioButton5.Text = "rashiRadioButton5";
+            this.rashiRadioButton5.UnCheckColor = System.Drawing.Color.Gray;
+            this.rashiRadioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 460);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(249, 15);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "* 매주 일요일에 일정이 등록됩니다.";
             // 
             // ScheduleEvent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(454, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(574, 560);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
             this.Name = "ScheduleEvent";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "용수 시험자 관리";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MonthCalendar monthCalendar2;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private Calendar.NETDemo.RashiRadioButton rashiRadioButton5;
+        private Calendar.NETDemo.RashiRadioButton rashiRadioButton4;
+        private Calendar.NETDemo.RashiRadioButton rashiRadioButton3;
+        private Calendar.NETDemo.RashiRadioButton rashiRadioButton2;
+        private Calendar.NETDemo.RashiRadioButton rashiRadioButton1;
+        private System.Windows.Forms.Label label7;
     }
 }
